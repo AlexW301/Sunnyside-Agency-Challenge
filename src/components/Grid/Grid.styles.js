d72caa94fe41ce03bb7c0d79a127e2230f7ce8d7
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-10-14 21:02:44 -0400

    Update Grid.styles.js

--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -13,6 +13,59 @@ export const Content = styled.div`
 
 export const TextBox = styled.div`
     height: 600px;
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    justify-content: center;
+    padding: 0 6rem;
+
+    h1 {
+        color: #23303e;
+        font-size: 2.5rem;
+        text-align: left;
+        margin-bottom: 1rem;
+    }
+
+    p {
+        color: hsl(213, 9%, 39%);
+        font-size: 1.2rem;
+        text-align: left;
+        line-height: 1.8rem;
+        color: #818498;
+        margin-bottom: 2.5rem;
+    }
+
+    .learn-more::after {
+        content: "";
+        background-color: #fad400;
+        opacity: .3;
+        z-index: -2;
+        position: absolute;
+        height: 7px;
+        width: 120px;
+        bottom: 0;
+        left: -5px;
+    }
+
+    .learn-more:link,
+    .learn-more:visited {
+        text-decoration: none;
+        color: #23303e;
+        text-transform: uppercase;
+        font-size: 1rem;
+        font-weight: 900;
+        position: relative;
+        width: 8.5rem;
+    }
+
+    .learn-more:hover,
+    .learn-more:active {
+        text-decoration: none;
+    }
+
+    .learn-more:hover::after {
+        opacity: .8;
+    }
 `;
 
 export const ImageBox = styled.img`
